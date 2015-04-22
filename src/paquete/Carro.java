336ebf421be5d6d9233ab7df5f9398eb18fe29a5
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-04-21 23:11:00 -0500

    Carros IO Finished

--- src/paquete/Carro.java
+++ src/paquete/Carro.java
@@ -4,4 +4,51 @@ package paquete;
  * Created by carlosrojas on 4/21/15.
  */
 public class Carro {
+    private String Marca;
+    private int Puertas;
+    private int Velocidad;
+    private String Color;
+
+    Carro(){
+
+        this.Marca = "No definida";
+        this.Puertas = 0;
+        this.Velocidad = 0;
+        this.Color = "No definida";
+    }
+
+    Carro(String Marca, int Puertas, int Velocidad, String Color){
+        this.Marca = Marca;
+        this.Puertas = Puertas;
+        this.Velocidad = Velocidad;
+        this.Color = Color;
+    }
+
+    public void setMarca(String Marca){
+        this.Marca = Marca;
+    }
+
+    public String getMarca(){
+        return this.Marca;
+    }
+
+    public void setPuertas(int Puertas){
+        this.Marca = Marca;
+    }
+
+    public int getPuertas(){
+        return this.Puertas;
+    }
+
+
+    public void ImprimirCarro(){
+        System.out.println(
+                "Mi carro::: \n" +
+                        "Marca: " + this.Marca + "\n" +
+                        "Puertas: " + this.Puertas + "\n" +
+                        "Velocidad: " + this.Velocidad + "\n" +
+                        "Color: " + this.Color
+        );
+    }
+
 }
--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -1,7 +1,44 @@
 package paquete;
+import java.util.Scanner;
 
 /**
  * Created by carlosrojas on 4/21/15.
  */
+
 public class Main {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        // Variables
+        String mar,col;
+        int puertas, vel;
+        // Marca
+
+        System.out.print("Introduzca Marca: ");
+        mar = sc.nextLine();
+
+
+        // Color
+        System.out.print("Introduzca el Color: ");
+        col = sc.nextLine();
+
+        //Puertas
+
+        System.out.print("Introduzca numero de puertas: ");
+        puertas = sc.nextInt();
+
+        //Velocidad
+        System.out.print("Introduzca Maxima Velocidad: ");
+        vel = sc.nextInt();
+
+
+
+        Carro Micarro = new Carro();
+        Micarro.setMarca(mar);
+        Micarro.setPuertas(puertas);
+        Micarro.ImprimirCarro();
+
+    }
 }
