7bd8dddd7f147fd83f057258430d0c9c7ac52f01
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2015-04-21 19:24:01 -0500

    Hello World Finished

---
+++ src/paquete/Main.java
@@ -0,0 +1,11 @@
+package paquete;
+
+/**
+ * Created by carlosrojas on 4/21/15.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hola Mundo !!!");
+    }
+}
